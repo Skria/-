676605364ad630a7b60b702c916adabb810ca047
Skria <673184662@qq.com>
2019-04-24 15:56:45 +0800

    初始化

---
+++ LuaFramework_UGUI-master/Assets/TestC/SMain.cs
@@ -0,0 +1,32 @@
+﻿using LuaFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddManager();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AddManager()
+    {
+        AppFacade.Instance.AddManager<LuaManager>(ManagerName.Lua);
+        //AppFacade.Instance.AddManager<PanelManager>(ManagerName.Panel);
+        //AppFacade.Instance.AddManager<SoundManager>(ManagerName.Sound);
+        //AppFacade.Instance.AddManager<TimerManager>(ManagerName.Timer);
+        //AppFacade.Instance.AddManager<NetworkManager>(ManagerName.Network);
+        //AppFacade.Instance.AddManager<ResourceManager>(ManagerName.Resource);
+        //AppFacade.Instance.AddManager<ThreadManager>(ManagerName.Thread);
+        //AppFacade.Instance.AddManager<ObjectPoolManager>(ManagerName.ObjectPool);
+        //AppFacade.Instance.AddManager<GameManager>(ManagerName.Game);
+    }
+}
